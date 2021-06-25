e8b9b03b9182e39b881d18c00648fe7810a50b5f
Deveshb15 <deveshb583@gmail.com>
2021-06-25 20:09:40 +0530

    Caching issue fixed

--- index.js
+++ index.js
@@ -29,8 +29,6 @@ let image_url = [];
 //check below drawit()
 let lastDrawImage = 0;
 
-let drawTimer;
-
 //function to download image
 const download_image = (url, image_path) =>
 axios({
@@ -46,7 +44,43 @@ axios({
     }),
 );
 
+  // function to draw image and post it
+  async function drawImage(back, img1, img2, img3){
+    //Creating an array so it becomes easier to Promise.all instead of one at a time
+    //Would love to see if you have any other approach to this, can't think of anything else
+    let imgArr = [back, img1, img2, img3];
+  
+    let jimps = [];
+  
+    //Read the image in jimp and push it to jimps array 
+    imgArr.forEach(image => jimps.push(jimp.read(image)));
+  
+    // fetch all the images
+    Promise.all(jimps).then(data => {
+      return Promise.all(jimps)
+    }).then(data => {
+      // composite the images on one another
+      data[0].composite(data[1],1070,50); //Your banner is 1500x500px, so change this pixels accordingly
+      data[0].composite(data[2],1160,50); //place the images wherever you want on the banner
+      data[0].composite(data[3],1250,50); //experiment with it or DM me on Twitter @Deveshb15 if you want any help
+  
+      // Write the image and save it
+      data[0].write('1500x500.png', function(){
+        console.log("done");
+      })
+    })
+  
+    // encode to base64 to post the image
+    const base64 = await fs.readFileSync('1500x500.png', { encoding: 'base64' });
+    // console.log(base64);
+  
+    // Update the banner
+    await twitterClient.accountsAndUsers.accountUpdateProfileBanner({banner: base64});
+  }
+
 async function start() {
+
+  const name = Math.random();
   const params = {
     screen_name: TWITTER_HANDLE, //name of twitter account
     count: 3                     //number of followers to be fetched
@@ -61,67 +95,40 @@ async function start() {
   
   (async () => {
     //download the image
-    await download_image(image_url[0], `img1.png`)
-    await download_image(image_url[1], `img2.png`)
-    await download_image(image_url[2], `img3.png`)
+    await download_image(image_url[0], `${name}-1.png`)
+    await download_image(image_url[1], `${name}-2.png`)
+    await download_image(image_url[2], `${name}-3.png`)
 
   async function drawit() {
     lastDrawImage = Date.now();
     // Draw the image and Post it
-    await drawImage('1500x500.png' ,`img1.png`,`img2.png`,`img3.png`);
+    await drawImage('1500x500.png' ,`${name}-1.png`,`${name}-2.png`,`${name}-3.png`);
+  }
+  async function deleteImg() {
+    try{
+      console.log('removing', `${name}{,1,2}.png`);
+      await fs.unlinkSync(`${name}-1.png`);
+      await fs.unlinkSync(`${name}-2.png`);
+      await fs.unlinkSync(`${name}-3.png`);
+    }catch(e){
+      console.log(e);
+    }
   }
   const remaining = Date.now() - lastDrawImage;
-
+  
   // Avoid hitting rate limit when update banner
   // 30 requests per 15 mins meaning 1 request per 30 secs
   if (remaining > 30000) {
     await drawit();
-  } else {
-    console.log('set timer', 30000 - remaining);
-    clearTimeout(drawTimer);
-    drawTimer = setTimeout(drawit, 30000 - remaining);
   }
+  await deleteImg();
 
-  })();
+})();
 
-}
-
-// function to draw image and post it
-async function drawImage(back, img1, img2, img3){
-  //Creating an array so it becomes easier to Promise.all instead of one at a time
-  //Would love to see if you have any other approach to this, can't think of anything else
-  let imgArr = [back, img1, img2, img3];
-
-  let jimps = [];
-
-  //Read the image in jimp and push it to jimps array 
-  imgArr.forEach(image => jimps.push(jimp.read(image)));
-
-  // fetch all the images
-  Promise.all(jimps).then(data => {
-    return Promise.all(jimps)
-  }).then(data => {
-    // composite the images on one another
-    data[0].composite(data[1],1070,50); //Your banner is 1500x500px, so change this pixels accordingly
-    data[0].composite(data[2],1160,50); //place the images wherever you want on the banner
-    data[0].composite(data[3],1250,50); //experiment with it or DM me on Twitter @Deveshb15 if you want any help
-
-    // Write the image and save it
-    data[0].write('1500x500.png', function(){
-      console.log("done");
-    })
-  })
-
-  // encode to base64 to post the image
-  const base64 = await fs.readFileSync('1500x500.png', { encoding: 'base64' });
-  // console.log(base64);
-
-  // Update the banner
-  await twitterClient.accountsAndUsers.accountUpdateProfileBanner({banner: base64})
 }
 
 // start everything
 start();
 setInterval(() => {
-  start();
+  start(); 
 }, 6000);
